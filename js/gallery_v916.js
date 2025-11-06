0981b458947279937133fd87619dff1e309c4ad3
qooqoo72526-boop <qooqoo72526@gmail.com>
2025-11-06 17:57:10 +0800

    Delete js/gallery_v916.js

--- js/gallery_v916.js
+++
@@ -1,6 +0,0 @@
-
-// keep using previous main_v915 starfield; here only set texts if needed
-(function(){
-  const t=document.querySelector('#gallery-title'); if(t) t.textContent='🔭 觀星畫廊 | Starflow Diary';
-  const s=document.querySelector('#gallery-subtitle'); if(s) s.textContent='他們的日常，你的宇宙';
-})();
